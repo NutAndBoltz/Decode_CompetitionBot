462c59c57b5bc33cdaf3aee1f44728ad331dca30
Adham El-Gabry <adhamelgabery@gmail.com>
2025-11-08 09:25:19 -0600

    Refactor RobotHardware class and fix launcher initialization

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotHardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotHardware.java
@@ -2,7 +2,7 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.Servo;
+
 public class RobotHardware {
 
     private LinearOpMode myOpMode = null;
@@ -13,21 +13,20 @@ public class RobotHardware {
     public DcMotor backRightDrive   = null;
 
     public DcMotor intake           = null;
-
-    public DcMotor launcher = null;
+    public DcMotor launcher         = null;
 
     public RobotHardware(LinearOpMode opmode) {
         myOpMode = opmode;
     }
 
-    public void init()    {
+    public void init() {
 
         frontLeftDrive  = myOpMode.hardwareMap.get(DcMotor.class, "FL_drive");
         frontRightDrive = myOpMode.hardwareMap.get(DcMotor.class, "FR_drive");
         backLeftDrive   = myOpMode.hardwareMap.get(DcMotor.class, "BL_drive");
         backRightDrive  = myOpMode.hardwareMap.get(DcMotor.class, "BR_drive");
 
-        intake = myOpMode.hardwareMap.get(DcMotor.class, "intake");
+        intake   = myOpMode.hardwareMap.get(DcMotor.class, "intake");
         launcher = myOpMode.hardwareMap.get(DcMotor.class, "launcher");
 
         frontLeftDrive.setDirection(DcMotor.Direction.REVERSE);
@@ -36,10 +35,9 @@ public class RobotHardware {
         backRightDrive.setDirection(DcMotor.Direction.FORWARD);
 
         intake.setDirection(DcMotor.Direction.FORWARD);
-        launcher.setDirection(DcMotor.Direction.FORWARD);      //Make reverse if necessary
+        launcher.setDirection(DcMotor.Direction.FORWARD); 
 
         myOpMode.telemetry.addData(">", "Hardware Initialized");
         myOpMode.telemetry.update();
     }
-
 }
